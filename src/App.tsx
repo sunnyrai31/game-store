d78d78e59d63ed716710d1a345d579ca179b1546
sunnyrai <sunnyrai@fico.com>
2024-02-16 18:45:59 +0530

    vercel build failure refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 
 import React, { useEffect, useState } from 'react';
 import './App.css'
-import HeaderComponent from './components/Header'
+import HeaderComponent from './components/HeaderComponent'
 import LeftPanelComponent from './components/LeftPanelComponent';
 import { fetchGenres, fetchAllTitle } from './services/ProductService';
 import TitlePanelComponent from './components/TitlePannelComponent';
